1ce071997fce7d0569815a5624cc213c90e41939
Jeferson Cruz <dev.blackfishlabs@gmail.com>
2020-01-30 16:31:36 -0400

    Update SDK

--- src/main/java/io/github/blackfishlabs/fiscal4desktop/NFeBuildAllCacerts.java
+++ src/main/java/io/github/blackfishlabs/fiscal4desktop/NFeBuildAllCacerts.java
@@ -1,6 +1,5 @@
 package io.github.blackfishlabs.fiscal4desktop;
 
-import br.indie.fiscal4j.DFAmbiente;
 import br.indie.fiscal4j.utils.DFCadeiaCertificados;
 import io.github.blackfishlabs.fiscal4desktop.common.properties.FiscalProperties;
 import org.apache.commons.io.FileUtils;
@@ -23,11 +22,11 @@ public class NFeBuildAllCacerts extends DFCadeiaCertificados {
             final NFeBuildAllCacerts buildCacerts = new NFeBuildAllCacerts(FiscalProperties
                     .getInstance()
                     .getDirCertificate()
-                    .concat("production.jks"));
+                    .concat("cacerts.jks"));
 
             FileUtils.writeByteArrayToFile(
                     new File(buildCacerts.getPath()),
-                    geraCadeiaCertificados(DFAmbiente.PRODUCAO, buildCacerts.getPassphrase()));
+                    geraCadeiaCertificados(buildCacerts.getPassphrase()));
             LOGGER.info("Certificate sucessfully generated in " + buildCacerts.getPath());
         } catch (final Exception e) {
             e.printStackTrace();
